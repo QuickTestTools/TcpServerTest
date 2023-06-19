24ae60f29691b204dfbaa1f014d1109daeff6c3d
张鹏 <scbeta@qq.com>
2023-06-19 09:16:08 +0800

    Release version 1.0.0

---
+++ src/TcpServerTest/Program.cs
@@ -0,0 +1,85 @@
+﻿using TcpServerTest;
+using Quick.Localize;
+using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+
+var textManager = TextManager.DefaultInstance;
+var title = textManager.GetText(Texts.Title);
+var version = "1.0.0";
+Console.Title = title;
+Console.WriteLine(textManager.GetText(Texts.WelcomeText, title, version));
+Console.WriteLine(textManager.GetText(Texts.RepoUrl, "https://github.com/QuickTestTools/TcpServerTest"));
+
+string ipAddress = null;
+int port = 0;
+
+if (args.Length >= 2)
+{
+    ipAddress = args[0];
+    port = int.Parse(args[1]);
+}
+else
+{
+    while (string.IsNullOrEmpty(ipAddress))
+    {
+        Console.Write(textManager.GetText(Texts.PleaseInputIpAddress));
+        var line = Console.ReadLine();
+        if (string.IsNullOrEmpty(line))
+        {
+            ipAddress = IPAddress.Any.ToString();
+            break;
+        }
+        if (IPAddress.TryParse(line, out _))
+            ipAddress = line;
+    }
+    while (port <= 0 || port > 65535)
+    {
+        Console.Write(textManager.GetText(Texts.PleaseInputPort));
+        var line = Console.ReadLine();
+        int.TryParse(line, out port);
+    }
+}
+
+var dateFormat = textManager.GetText(Texts.DateFormat);
+Console.Title = $"{ipAddress}:{port} - {title}";
+TcpListener tcpListener = new TcpListener(IPAddress.Parse(ipAddress), port);
+tcpListener.Start();
+
+
+Console.WriteLine(textManager.GetText(Texts.ExitProgramTip));
+while (true)
+{
+    var client = tcpListener.AcceptTcpClient();
+    var clientEndpoint = client.Client.RemoteEndPoint.ToString();
+    Console.WriteLine(textManager.GetText(Texts.NewClientConnected, clientEndpoint));
+    _ = Task.Run(() =>
+    {
+        byte[] buffer = new byte[1024];
+        using (var stream = client.GetStream())
+        {
+            try
+            {
+                while (true)
+                {
+                    var ret = stream.Read(buffer, 0, buffer.Length);
+                    if (ret <= 0)
+                        throw new IOException();
+                    var text = Encoding.Default.GetString(buffer, 0, ret);
+                    var hex = BitConverter.ToString(buffer, 0, ret);
+                    Console.WriteLine(textManager.GetText(Texts.ClientDataRecved, clientEndpoint, text, hex));
+                }
+            }
+            catch
+            {
+                Console.WriteLine(textManager.GetText(Texts.NewClientDisconnected, clientEndpoint));
+                client.Dispose();
+            }
+        }
+    });
+}
---
+++ src/TcpServerTest/Texts.cs
@@ -0,0 +1,39 @@
+﻿using Quick.Localize;
+
+namespace TcpServerTest
+{
+    [TextResource]
+    public enum Texts
+    {
+        [Text("en-US", "MM/dd/yyyy HH:mm:ss")]
+        [Text("zh-CN", "yyyy-MM-dd HH:mm:ss")]
+        DateFormat,
+        [Text("en-US", "TcpServer Test Tool")]
+        [Text("zh-CN", "TcpServer测试工具")]
+        Title,
+        [Text("en-US", "Welcome to use {0}_v{1}")]
+        [Text("zh-CN", "欢迎使用{0}_v{1}")]
+        WelcomeText,
+        [Text("en-US", "Repo Url:{0}")]
+        [Text("zh-CN", "仓库地址:{0}")]
+        RepoUrl,
+        [Text("en-US", "Please input IPAddress(Default:0.0.0.0):")]
+        [Text("zh-CN", "请输入IP地址(默认:0.0.0.0)：")]
+        PleaseInputIpAddress,
+        [Text("en-US", "Please input listen port:")]
+        [Text("zh-CN", "请输入监听端口：")]
+        PleaseInputPort,
+        [Text("en-US", "[Press Ctrl+C to exit]")]
+        [Text("zh-CN", "[按Ctrl+C终止程序]")]
+        ExitProgramTip,
+        [Text("en-US", "[{0}]Connected")]
+        [Text("zh-CN", "[{0}]已连接")]
+        NewClientConnected,
+        [Text("en-US", "[{0}]Disconnected")]
+        [Text("zh-CN", "[{0}]已断开")]
+        NewClientDisconnected,
+        [Text("en-US", "[{0}][RX]\r\nText: {1}\r\nHex:{2}")]
+        [Text("zh-CN", "[{0}][收到数据]\r\n文本: {1}\r\n16进制: {2}")]
+        ClientDataRecved
+    }
+}
